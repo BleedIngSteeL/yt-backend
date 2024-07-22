004518523a3e7f0883148ae3dea79bcea613095a
Aman <amankumar.ckss@gmail.com>
2024-07-23 04:15:23 +0530

    utils folder upated with : asyncHandler, ApiErrors, ApiResponse

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import dotenv from "dotenv"
 
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 // dotenv ko require se import banate h aise 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed",err)
+})
